1f7d247a2433a8be5389d7e6a56830b8cd21fc3e
Ilya Brant <detroitib31@gmail.com>
2024-05-08 19:04:18 +0600

    Добавили слушатель нажатий

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -15,6 +20,25 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val buttonSearch: Button = findViewById(R.id.button_search)
+        val buttonMediaLibrary: Button = findViewById(R.id.button_media_library)
+        val buttonSettings: Button = findViewById(R.id.button_settings)
+
+        val buttonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на кнопку Поиск!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        buttonSearch.setOnClickListener(buttonClickListener)
+
+        buttonMediaLibrary.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку Медиатека!", Toast.LENGTH_SHORT).show()
+        }
+
+        buttonSettings.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку Настройки!", Toast.LENGTH_SHORT).show()
         }
     }
 }
