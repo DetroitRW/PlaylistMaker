0c09dc29c6ed432eb184e56a7fd16c98ad1f156f
Ilya Brant <detroitib31@gmail.com>
2024-05-28 15:07:35 +0600

    Добавил реализацию Switch

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,17 +1,26 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Switch
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
+import androidx.core.content.ContextCompat.startActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SettingsActivity: AppCompatActivity() {
+    companion object {
+        const val NIGHT = "night"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,15 +30,33 @@ class SettingsActivity: AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val switchThemes: Button = findViewById(R.id.switchThemes)
+        val switchThemes: Switch = findViewById(R.id.switchThemes)
         val imageViewBack: ImageView = findViewById(R.id.imageViewBack)
         val imageViewShare: ImageView = findViewById(R.id.imageViewShare)
         val imageViewSupport: ImageView = findViewById(R.id.imageViewSupport)
         val imageViewArrowNext: ImageView = findViewById(R.id.imageViewArrowNext)
 
-        switchThemes.setOnClickListener {
+        val sharedPreferences = getSharedPreferences("Mode", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        val nightMode = sharedPreferences.getBoolean(NIGHT, false)
 
+        if (nightMode) {
+            switchThemes.isChecked = true
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         }
+
+        switchThemes.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (!isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                editor.putBoolean(NIGHT,false)
+                editor.apply()
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                editor.putBoolean(NIGHT,true)
+                editor.apply()
+            }
+        }
+
 
         imageViewBack.setOnClickListener {
             finish()
