8a2bddb62dc4565e30db45397a5ee502314e1cff
Ilya Brant <detroitib31@gmail.com>
2024-05-28 06:54:52 +0600

    Сверстал экран поиска и добавил функционал

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,10 +1,18 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isInvisible
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +24,43 @@ class SearchActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val imageViewBack: ImageView = findViewById(R.id.imageViewBack)
+        val imageViewReset: ImageView = findViewById(R.id.imageViewReset)
+        val editTextSearch: EditText = findViewById(R.id.editTextSearch)
+
+        imageViewBack.setOnClickListener {
+            finish()
+        }
+
+        imageViewReset.setOnClickListener {
+            editTextSearch.setText("")
+            hideKeyboard()
+        }
+
+        editTextSearch.setOnClickListener {
+        }
+
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+                    imageViewReset.visibility = View.INVISIBLE
+                } else {
+                    imageViewReset.visibility = View.VISIBLE
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+        editTextSearch.addTextChangedListener(simpleTextWatcher)
+    }
+
+    private fun hideKeyboard() {
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
+        inputMethodManager?.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
     }
 }
